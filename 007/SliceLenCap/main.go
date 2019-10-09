88f08c0a2e3743642ed07b32ab03ee5ba55fc2cc
youguanxinqing <youguanxinqing@qq.com>
2019-10-09 06:41:01 -0400

    019.10.9 18:40

---
+++ 007/SliceLenCap/main.go
@@ -0,0 +1,25 @@
+package main
+ 
+import "fmt"
+ 
+func main() {
+	s1 := make([]int, 5)
+	fmt.Printf("The length of s1: %d\n", len(s1))
+	fmt.Printf("The capacity of s1: %d\n", cap(s1))
+	fmt.Printf("The value of s1: %d\n", s1)
+	fmt.Printf("\n")
+	
+	s2 := make([]int, 5, 8)
+	fmt.Printf("The length of s2: %d\n", len(s2))
+	fmt.Printf("The capacity of s2: %d\n", cap(s2))
+	fmt.Printf("The value of s2: %d\n", s2)
+	fmt.Printf("\n")
+
+	s3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	s4 := s3[3:6]
+	fmt.Printf("The capacity of s3: %d\n", cap(s3))
+	fmt.Printf("The length of s4: %d\n", len(s4))
+	fmt.Printf("The capacity of s4: %d\n", cap(s4))
+	fmt.Printf("The value of s4: %d\n", s4)
+
+}
